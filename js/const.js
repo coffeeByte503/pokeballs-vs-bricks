0dcfd022fc7238cf0da6dae9f051a37a6f0fc5ea
Calderon <49690606+coffeeByte503@users.noreply.github.com>
2025-05-02 19:13:25 -0600

    Update const.js

--- js/const.js
+++ js/const.js
@@ -1,3 +1,3 @@
 export const CANVAS_WIDTH = 300;
 export const CANVAS_HEIGHT = 500;
-export const SERVER = "https://luis72353.github.io/pokeballs-vs-bricks/";
+export const SERVER = "https://coffeebyte503.github.io/";
